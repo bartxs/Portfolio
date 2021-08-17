368fd9846b7e8d7ee875f81d8882e55254d5075d
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-08-17 20:34:53 +0200

    Responsive layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,19 +71,19 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /* PORTFOLIO SWIPER */
-let swiperPortfolio = new Swiper('.portfolio_container', {
-    cssMode: true,
-    loop: true,
+// let swiperPortfolio = new Swiper('.portfolio_container', {
+//     cssMode: true,
+//     loop: true,
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-});
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+// });
 
 /* SCROLL SECTION ACTIVE LINK */
 const sections = document.querySelectorAll('section[id]')
