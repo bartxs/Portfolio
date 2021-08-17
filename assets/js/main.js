fb5465233c64fed6acbb656c58af95e3c31f5856
Bartosz Soltysik <b.soltysik90@gmail.com>
2021-08-17 13:47:04 +0200

    logo change

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,7 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /* PORTFOLIO SWIPER */
-let swiper = new Swiper('.portfolio_container', {
+let swiperPortfolio = new Swiper('.portfolio_container', {
     cssMode: true,
     loop: true,
 
